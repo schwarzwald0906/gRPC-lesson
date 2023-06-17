27b2b9107f7a3e80c642788ada87a39fb299bff1
schwarzwald0906 <schwarzwald0906@gmail.com>
2023-06-17 21:49:50 +0900

    unaryPCのクライアント実装

---
+++ server/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"grpc-lesson/pb"
+	"io/ioutil"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+type server struct {
+	pb.UnimplementedFileServiceServer
+}
+
+func (*server) ListFiles(ctx context.Context, req *pb.ListFilesRequest) (*pb.ListFilesResponse, error) {
+	fmt.Println("ListFiles was invoked")
+
+	dir := "/Users/kimsukhong/git/grpc-lesson/storage"
+	paths, err := ioutil.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+	var filenames []string
+	for _, path := range paths {
+		if !path.IsDir() {
+			filenames = append(filenames, path.Name())
+		}
+	}
+
+	res := &pb.ListFilesResponse{
+		Filenames: filenames,
+	}
+	return res, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", "localhost:50051")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterFileServiceServer(s, &server{})
+
+	fmt.Println("server listening on")
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
+}
